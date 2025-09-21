0cc713394caf6695317596d6b3634397504621fb
unknown <daviidxestrada@gmail.com>
2025-09-21 11:48:30 +0200

    Manejo del DOMv1

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ cajaRojo.forEach((item)=>{
 })
 
 
-const clase = caja1.getAttribute("class")
+/* const clase = caja1.getAttribute("class")
 console.log(clase)
 
 caja1.setAttribute("data-forma", "redondo")
@@ -32,11 +32,11 @@ const nombre = document.querySelector("#nombre")
 console.log(nombre.value)
 
 const cajaContenido = document.querySelector("#cajaContenido")
-console.log(cajaContenido)
+console.log(cajaContenido) */
 
 
 const contenidoDeCajaContenido=cajaContenido.innerHTML
-console.log(contenidoDeCajaContenido)
+/* console.log(contenidoDeCajaContenido)
 
 // cajaContenido.innerHTML="<h1>Nuevo contenido</h1>"
 
@@ -44,3 +44,39 @@ const textDeCajaContenido = cajaContenido.textContent
 console.log(textDeCajaContenido)
 
 cajaContenido.textContent = "Nuevo contenido"
+
+console.log(cajaContenido.classList)
+
+cajaContenido.classList.remove("otra", "verde")
+console.log(cajaContenido)
+
+cajaContenido.classList.add("grande", "redondo")
+console.log(cajaContenido)
+
+cajaContenido.classList.replace("grande","pequenia")
+console.log(cajaContenido)
+
+let contieneClase = cajaContenido.classList.contains("redondo")
+console.log(contieneClase)
+
+cajaContenido.classList.toggle("redondo")
+console.log(cajaContenido) */
+
+const elementoNuevo = document.createElement("section")
+elementoNuevo.classList.add("rojo")
+elementoNuevo.innerHTML=`<h1>Contenido de elemento nuevo</h1>`
+console.log(elementoNuevo)
+
+cajaContenido.before(elementoNuevo)
+
+const articulo = document.querySelector("#articulo1")
+const lista = document.querySelector("#lista")
+
+articulo.remove()
+
+console.log(lista.children[0])
+
+lista.removeChild(lista.children[0])
+lista.removeChild(lista.children[1])
+
+lista.innerHTML=""
